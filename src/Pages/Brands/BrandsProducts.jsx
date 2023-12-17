0fd6cb55c33753f608d8bddd85744708d3081426
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-17 20:40:10 +0600

    brand route added

--- src/Pages/Brands/BrandsProducts.jsx
+++ src/Pages/Brands/BrandsProducts.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import UseAxiosPublic from "../../Hooks/AxiosPublic/UseAxiosPublic";
+import useFeatureProducts from "../../Hooks/useFeature/useFeatureProducts";
+import DisplayProducts from "../../Components/DispalyProducts/DisplayProducts";
 
 const BrandsProducts = () => {
-    const {name} = useParams()
-    const 
+    const { name } = useParams()
+    const [brand, setBrand] = useState([])
+    const axiosPublic = UseAxiosPublic()
+    const [feature] = useFeatureProducts()
+    useEffect(() => {
+        const brandName = feature?.filter(i => i.brand === name)
+        setBrand(brandName)
+    }, [axiosPublic, name, feature])
+
+    console.log(name, brand)
 
     return (
-        <div>
-
+        <div className="my-20">
+            <div className="grid grid-cols-1 gap-10 items-center md:grid-cols-2 lg:grid-cols-3">
+                {
+                    brand?.map(i => <DisplayProducts key={i._id} products={i}></DisplayProducts>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Brands/Brands.jsx
+++ src/Pages/Home/Brands/Brands.jsx
@@ -6,22 +6,22 @@ const Brands = () => {
         <div className="my-20">
             <SectionTittle subTittle={'Brands'} tittle={"Popular Brands"}></SectionTittle>
             <div className="grid grid-cols-4 items-center  my-10">
-                <Link to={`/brands/${'adidas'}`} className="col-span-3  w-full">
+                <Link to={`/brands/${'Adidas'}`} className="col-span-3  w-full">
                     <img className=" h-[300px]  w-full" src="https://i.ibb.co/9hmWysy/download-1.png" alt="" />
                 </Link>
-                <Link to={`/brands/${'adidas'}`}>
+                <Link to={`/brands/${'Calvin Klein'}`}>
                     <img className=" h-[300px] mx-auto  w-full" src="https://i.ibb.co/6Xhns5T/download-2.png" alt="" />
                 </Link>
-                <Link to={`/brands/${'adidas'}`}>
+                <Link to={`/brands/${'ZARA'}`}>
                     <img className=" h-[300px] mx-auto w-full" src="https://i.ibb.co/yXDBNnZ/download-3.png" alt="" />
                 </Link>
-                <Link to={`/brands/${'adidas'}`} className="col-span-3 w-full"> 
+                <Link to={`/brands/${'Gucci'}`} className="col-span-3 w-full"> 
                     <img className=" h-[300px] mx-auto col-span-3 w-full" src="https://i.ibb.co/Lhs8gY4/download.jpg" alt="" />
                 </Link>
-                <Link to={`/brands/${'adidas'}`} className="col-span-2">
+                <Link to={`/brands/${'Louis Vuitton'}`} className="col-span-2">
                     <img className=" h-[300px] mx-auto w-full" src="https://i.ibb.co/sy8bdD8/download.jpg" alt="" />
                 </Link>
-                <Link to={`/brands/${'adidas'}`} className="col-span-2">
+                <Link to={`/brands/${'Prada'}`} className="col-span-2">
                     <img className=" h-[300px] mx-auto w-full " src="https://i.ibb.co/g6jFm0f/download-5.png" alt="" />
                 </Link>
             </div>
